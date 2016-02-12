d15582687a21b33c6544fa781b1057c8e7636e15
Bouchiba <bouchibahassan@gmail.com>
2016-02-12 10:09:40 +0100

    correct indentation.

--- tps/tp_boids/applications/app_boids.cc
+++ tps/tp_boids/applications/app_boids.cc
@@ -10,30 +10,29 @@ int WINDOW_WIDTH = 200;
 int WINDOW_HEIGHT = 200;
 
 void init(void) {
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_NORMALIZE);
-	glShadeModel(GL_FLAT);
-
-        //More init
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_COLOR_MATERIAL);
+  glEnable(GL_NORMALIZE);
+  glShadeModel(GL_FLAT);
+  //More init
 }
 
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0, 3.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  gluLookAt(0.0, 3.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-        //Display functions
+  //Display functions
 
-	glutSwapBuffers();
+  glutSwapBuffers();
 }
 
 void timer(int v) {
-	glutPostRedisplay();
-	glutTimerFunc(1000 / FPS, timer, 0);
+  glutPostRedisplay();
+  glutTimerFunc(1000 / FPS, timer, 0);
 }
 
 void animateScene(){
@@ -41,34 +40,34 @@ void animateScene(){
 }
 
 void reshape(int w, int h) {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60, (GLfloat)w / (GLfloat)h, 1.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+  glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(60, (GLfloat)w / (GLfloat)h, 1.0, 100.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 }
 
 void processKeys(unsigned char key, int x, int y) {
 
 }
 
-/* Main function: GLUT runs as a console application starting at main()  */
+// Main function: GLUT runs as a console application starting at main()
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutInitWindowPosition(WINDOW_X, WINDOW_Y);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Premiere fenetre GLUT");
+    glutInit(&argc, argv);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  glutInitWindowPosition(WINDOW_X, WINDOW_Y);
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+  glutCreateWindow("Premiere fenetre GLUT");
 
-	init();
-	glutDisplayFunc(display);
-	glutIdleFunc(animateScene);
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(processKeys);
-	glutTimerFunc(1000 / FPS, timer, 0);
+  init();
+  glutDisplayFunc(display);
+  glutIdleFunc(animateScene);
+  glutReshapeFunc(reshape);
+  glutKeyboardFunc(processKeys);
+  glutTimerFunc(1000 / FPS, timer, 0);
 
-	glutMainLoop();
-	return 0;
+  glutMainLoop();
+  return 0;
 }
